51bdd31ba9c234c80da59badf15e465a354b71e5
Jannik Menzel <jnk.menzel@gmail.com>
2026-03-21 00:42:21 +0100

    feat: add page titles and dark mode toggle functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -6,6 +7,7 @@ export default defineConfig({
     site: "https://test.jannikmenzel.me",
     base: "/",
     output: "static",
+    integrations: [icon()],
     vite: {
         plugins: [tailwindcss()],
     },
